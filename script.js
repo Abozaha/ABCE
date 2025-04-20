3c29e51804ec1b8613111d653124d3d171c95694
Abozaha <awab3410@gmail.com>
2025-04-20 11:10:50 -0400

    Added clear button functionality

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function fetchCountry() {
-    const countryName = document.getElementById('countryInput').value;
-    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-        .then(response => response.json())
-        .then(data => {
-            const country = data[0];
-            document.getElementById('countryInfo').innerHTML = `
-                <h2>${country.name.common}</h2>
-                <p>Capital: ${country.capital[0]}</p>
-                <p>Population: ${country.population}</p>
-                <img src="${country.flags.png}" alt="Flag" width="100">
-            `;
-        })
-        .catch(error => {
-            document.getElementById('countryInfo').innerHTML = '<p>Country not found!</p>';
-        });
+function clearInfo() {
+    document.getElementById('countryInfo').innerHTML = '';
+    document.getElementById('countryInput').value = '';
 }
